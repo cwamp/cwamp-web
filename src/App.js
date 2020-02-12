fb2f78b6a17b8a2de5080ed6175d2dbc49f89e51
upcwangying <upcwangying@126.com>
2020-02-12 18:11:31 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://cwamp.cn"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Go to website
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
